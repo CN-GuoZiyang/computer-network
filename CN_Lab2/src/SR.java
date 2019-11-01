e12fb4baab1831d7bc18b65816cf7926e1cd957e
CN-GuoZiyang <guoziyang0033@gmail.com>
2019-11-01 15:29:35 +0800

    Lab2注释

--- CN_Lab2/src/SR.java
+++ CN_Lab2/src/SR.java
@@ -8,15 +8,21 @@ import java.net.UnknownHostException;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 可收发的SR协议实现
+ * 
+ * @author Ziyang Guo
+ */
 public class SR {
     
-    private InetAddress host;
-    private int targetPort, myPort;
-    private int windowSize = 16;
-    private int sendMaxTime = 2;
-    private int receiveMaxTime = 4;
-    private long base = 0;
-    private int loss = 10;
+    private InetAddress host;   // 目的主机地址
+    private int targetPort; // 目的端口
+    private int myPort;     // 本地端口
+    private int windowSize = 16;    // 窗口大小
+    private int sendMaxTime = 2;    // 最大发送次数
+    private int receiveMaxTime = 4; // 最大接收次数
+    private long base = 0;          // 窗口base序号
+    private int loss = 10;          // 模拟丢包
 
     public SR(String host, int targetPort, int myPort) throws UnknownHostException {
         this.myPort = myPort;
@@ -24,20 +30,28 @@ public class SR {
         this.host = InetAddress.getByName(host);
     }
 
+    /**
+     * 向目的主机端口发送内容
+     * 
+     * @param content 待发送的内容
+     * @throws IOException IO异常
+     */
     public void send(byte[] content) throws IOException {
-        int sendIndex = 0;
+        int sendIndex = 0;  // 发送到的字节序号
         int length;
-        int maxLength = 1024;
+        int maxLength = 1024;   // 最大数据长度
         DatagramSocket datagramSocket = new DatagramSocket(myPort);
-        List<ByteArrayOutputStream> datagramBuffer = new LinkedList<>();
-        List<Integer> timers = new LinkedList<>();
-        long sendSeq = base;
+        List<ByteArrayOutputStream> datagramBuffer = new LinkedList<>();    // 对当前窗口的内容进行缓存，方便重发
+        List<Integer> timers = new LinkedList<>();  // 当前窗口的数据帧已发送次数
+        long sendSeq = base;    // 发送的数据帧的序列号
         do {
-
+            // 循环将窗口发满
             while(timers.size() < windowSize && sendIndex < content.length && sendSeq < 256) {
                 timers.add(0);
                 datagramBuffer.add(new ByteArrayOutputStream());
                 length = Math.min(content.length - sendIndex, maxLength);
+
+                // 拼接数据帧，按照 base + seq + data 的顺序拼接
                 ByteArrayOutputStream one = new ByteArrayOutputStream();
                 byte[] temp = new byte[1];
                 temp[0] = new Long(base).byteValue();
@@ -46,24 +60,34 @@ public class SR {
                 temp[0] = new Long(sendSeq).byteValue();
                 one.write(temp, 0, 1);
                 one.write(content, sendIndex, length);
+
+                // 向目的主机发送
                 DatagramPacket packet = new DatagramPacket(one.toByteArray(), one.size(), host, targetPort);
                 datagramSocket.send(packet);
+
+                // 将发送的内容暂存在缓存中
                 datagramBuffer.get((int)(sendSeq - base)).write(content, sendIndex, length);
                 sendIndex += length;
                 System.out.println("发送数据包：base " + base + " seq " + sendSeq);
                 sendSeq ++;
             }
+            
+            // 设置超时时间1000ms
             datagramSocket.setSoTimeout(1000);
             DatagramPacket receivePacket;
+
+            // 循环从目的主机接收ack
             try {
                 while(!checkWindow(timers)) {
                     byte[] recv = new byte[1500];
                     receivePacket = new DatagramPacket(recv, recv.length);
                     datagramSocket.receive(receivePacket);
+                    // 取出ack的序列号
                     int ack = (int)((recv[0] & 0x0FF) - base);
                     timers.set(ack, -1);
                 }
             } catch (SocketTimeoutException e) {
+                // socket超时，设置确认次数+1
                 for(int i = 0; i < timers.size(); i ++) {
                     int tempTime = timers.get(i);
                     if(tempTime != -1) {
@@ -71,6 +95,7 @@ public class SR {
                     }
                 }
             }
+            // 重发所有超过最大确认次数的数据帧
             for(int i = 0; i < timers.size(); i ++) {
                 if(timers.get(i) > sendMaxTime) {
                     ByteArrayOutputStream resender = new ByteArrayOutputStream();
@@ -89,6 +114,7 @@ public class SR {
             }
             int i = 0;
             int s = timers.size();
+            // 确认并删除所有已经确认过的缓存
             while(i < s) {
                 if(timers.get(i) == -1) {
                     timers.remove(i);
@@ -99,6 +125,8 @@ public class SR {
                     break;
                 }
             }
+
+            // 更新发送序号
             if(base >= 256) {
                 base -= 256;
                 sendSeq -= 256;
@@ -108,32 +136,45 @@ public class SR {
         datagramSocket.close();
     }
 
+    /**
+     * 从目的主机接收
+     * 
+     * @return 接收到的有序的字节
+     * @throws IOException IO异常
+     */
     public ByteArrayOutputStream receive() throws IOException {
-        int count = 0;
-        int time = 0;
-        long max = 0;
-        long receiveBase = -1;
-        ByteArrayOutputStream result = new ByteArrayOutputStream();
-        DatagramSocket datagramSocket = new DatagramSocket(myPort);
+        int count = 0;  // 接收到的数据帧个数
+        int time = 0;   // 同一个数据帧的接收次数
+        long max = 0;   // 当前最大接收到的序列号 - base，乱序
+        long receiveBase = -1;  // 接收窗口的base
+        ByteArrayOutputStream result = new ByteArrayOutputStream(); // 按序输出流
+        DatagramSocket datagramSocket = new DatagramSocket(myPort); // server监听socket
         datagramSocket.setSoTimeout(1000);
-        List<ByteArrayOutputStream> datagramBuffer = new LinkedList<>();
+        List<ByteArrayOutputStream> datagramBuffer = new LinkedList<>();    // 缓存窗口内接收到的数据帧
         DatagramPacket receivePacket;
+        
         for(int i = 0; i < windowSize; i ++) {
             datagramBuffer.add(new ByteArrayOutputStream());
         }
+
         while (true) {
             try {
                 byte[] recv = new byte[1500];
                 receivePacket = new DatagramPacket(recv, recv.length, host, targetPort);
                 datagramSocket.receive(receivePacket);
+                // 模拟丢包，即接收之后不处理，当成没接收到
                 if(count % loss != 0) {
+                    // 提取出接收到的base和序列号
                     long base = recv[0] & 0x0FF;
                     long seq = recv[1] & 0x0FF;
                     if(receiveBase == -1) {
                         receiveBase = base;
                     }
+                    // 若发送端base更新（即已经确认了几个数据帧）
                     if(base != receiveBase) {
+                        // 从缓存中取出已经确认完成的数据帧拼接
                         ByteArrayOutputStream temp = getBytes(datagramBuffer, (base - receiveBase) > 0 ? (base - receiveBase) : max + 1);
+                        // 空出缓存
                         for(int i = 0; i < base - receiveBase; i ++) {
                             datagramBuffer.remove(0);
                             datagramBuffer.add(new ByteArrayOutputStream());
@@ -145,20 +186,23 @@ public class SR {
                     if(seq - base > max) {
                         max = seq - base;
                     }
+                    // 将接收到的数据帧写入缓存
                     ByteArrayOutputStream recvBytes = new ByteArrayOutputStream();
                     recvBytes.write(recv, 2, receivePacket.getLength() - 2);
                     datagramBuffer.set((int) (seq - base), recvBytes);
+                    // 返回ACK
                     recv = new byte[1];
                     recv[0] = new Long(seq).byteValue();
                     receivePacket = new DatagramPacket(recv, recv.length, host, targetPort);
                     datagramSocket.send(receivePacket);
-                    System.out.println("接收到数据包：base " + receiveBase + " seq " + seq);
+                    System.out.println("接收到数据包：base " + base + " seq " + seq);
                 }
                 count ++;
                 time = 0;
             } catch (SocketTimeoutException e) {
                 time ++;
             }
+            // 超出最大接收时间，则接收结束，写出数据
             if(time > receiveMaxTime) {
                 ByteArrayOutputStream temp = getBytes(datagramBuffer, max + 1);
                 result.write(temp.toByteArray(), 0, temp.size());
